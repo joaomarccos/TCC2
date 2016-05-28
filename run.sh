91c3c503a1b1d164b08d4dc473b5f468de48c9e7
joaomarccos <joaomarccos.ads@gmail.com>
2016-05-27 22:34:59 -0300

    correção até capitulo 3

---
+++ run.sh
@@ -0,0 +1,16 @@
+make
+echo "Compilou...  OK"
+echo "Aperte <ENTER> para continuar..."
+read #pausa até que o ENTER seja pressionado
+make ref
+echo "Verificou referências... OK"
+echo "Aperte <ENTER> para continuar..."
+read #pausa até que o ENTER seja pressionado
+make
+make
+echo "Linkou referencias... OK"
+echo "Aperte <ENTER> para continuar..."
+read #pausa até que o ENTER seja pressionado
+make pdf
+echo "Gerando PDF..."
+echo "OK"
