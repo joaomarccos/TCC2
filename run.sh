b3d48e150fc5a05972bddf8469a1e6a42a284e0c
joaomarccos <joaomarccos.ads@gmail.com>
2016-06-01 00:04:41 -0300

    Correções banca

--- run.sh
+++ run.sh
@@ -1,16 +1,10 @@
 make
 echo "Compilou...  OK"
-echo "Aperte <ENTER> para continuar..."
-read #pausa até que o ENTER seja pressionado
 make ref
 echo "Verificou referências... OK"
-echo "Aperte <ENTER> para continuar..."
-read #pausa até que o ENTER seja pressionado
 make
 make
 echo "Linkou referencias... OK"
-echo "Aperte <ENTER> para continuar..."
-read #pausa até que o ENTER seja pressionado
 make pdf
 echo "Gerando PDF..."
 echo "OK"
